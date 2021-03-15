72db34ba9199a593a50bc95dd5dcc3a95a81e8e0
iantendu <iantendu@gmail.com>
2021-03-15 19:18:20 +0300

    First commit

---
+++ app/src/main/java/com/example/geusscelebrity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.geusscelebrity;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private class DownloadTask extends AsyncTask<>
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
